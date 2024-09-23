fd4f7facbd446067fbf74135c2a5522f5637cabf
kimiyash <kimiyash@gmail.com>
2024-09-23 15:52:12 +0900

    ch04_15_slice1

--- ch04/examples/ch04_14_array/src/main.rs
+++ ch04/examples/ch04_14_array/src/main.rs
@@ -27,4 +27,43 @@ fn main() {
     assert_eq!(v1.len(), 101);
     assert_eq!(v1.pop(), Some(1));
     assert_eq!(v1.len(), 100);
+
+    let array1 = ['H', 'e', 'l', 'l', 'o'];
+    assert_eq!(array1[1], 'e');
+
+    let mut array2 = [0, 1, 2];
+    array2[1] = 10;
+    assert_eq!(array2, [0, 10, 2]);
+
+    // インデックスは定数でなくても良い
+    let mut index = 0;
+    assert_eq!(array2[index], 0);
+    index += 1;
+    assert_eq!(array2[index], 10);
+
+    let array3 = [0, 1];
+    // array3[2]; // コンパイルエラー、index out of bounds: the length is 2 but the index is 2
+
+    // 実行時にエラーになるケース
+    // let index = 2;
+    // array3[index];
+    // error: this operation will panic at runtime
+    //    --> src/main.rs:49:5
+    //    |
+    // 49 |     array3[index];
+    //    |     ^^^^^^^^^^^^^ index out of bounds: the length is 2 but the index is 2
+
+    assert_eq!(array3.get(1), Some(&1));
+    assert_eq!(array3.get(2), None);
+
+    let array4 = ['a'; 50];
+    for ch in array4.iter() {
+        print!("{}", *ch);
+    }
+
+    let mut array5 = [1; 50];
+    for n in array5.iter_mut() {
+        *n *= 2;
+    }
+
 }
