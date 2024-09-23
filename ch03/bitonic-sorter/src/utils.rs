5a0fa2e4e2a6f75de3e212beb6be12b7cf480801
kimiyash <kimiyash@gmail.com>
2024-09-23 11:08:15 +0900

    bitonic-sorter

--- ch03/bitonic-sorter/src/utils.rs
+++ ch03/bitonic-sorter/src/utils.rs
@@ -4,9 +4,5 @@ use rand_pcg::Pcg64Mcg;
 
 pub fn new_u32_vec(n: usize) -> Vec<u32> {
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
-    let mut v = Vec::with_capacity(n);
-    for _ in 0..n {
-        v.push(rng.sample(Standard));
-    }
-    v
+    rng.sample_iter(&Standard).take(n).collect()
 }
