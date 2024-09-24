5be80023b5898cd6633b0e2e8590029b387d8407
kimiyash <kimiyash@gmail.com>
2024-09-24 11:00:04 +0900

    ch05_05_string1

---
+++ ch05/example/ch05_05_string1/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut s1 = "ラズベリー".to_string();
+    let mut s2 = String::from("ブラックベリー");
+    // Rust 1.19 より前のバージョンでは性能上の理由から to_owned() が推奨されていたが、いまは関係なし
+    let s3 = "ストロベリー".to_owned();
+    s1.push_str("タルト");
+    assert_eq!(s1, "ラズベリータルト");
+    s2.push('と');
+    // push_strが受け付けるのは &str のみだが & をつけると型強制という仕組みで &String から &str へ変換される
+    s2.push_str(&s3);
+    assert_eq!(s2, "ブラックベリーとストロベリー");
+}
