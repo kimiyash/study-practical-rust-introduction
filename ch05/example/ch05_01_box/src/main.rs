dc7217fe4f0d79a6ca0e5deebd806459a8f6aa05
kimiyash <kimiyash@gmail.com>
2024-09-24 09:27:35 +0900

    ch05_01_box

---
+++ ch05/example/ch05_01_box/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let t1 = (3, "birds".to_string());
+    let mut b1 = Box::new(t1);
+    (*b1).0 += 1;
+    assert_eq!(*b1, (4, "birds".to_string()));
+    // println!("{:?}", t1); // コンパイルエラー、すでにBoxに所有権が移っている
+}
