a839a9e52be5ea014bbf369fdf98bd209b6ec156
kimiyash <kimiyash@gmail.com>
2024-09-25 11:55:37 +0900

    ch07_02_move_semantics

--- ch07/example/ch07_02_move_semantics/src/main.rs
+++ ch07/example/ch07_02_move_semantics/src/main.rs
@@ -16,16 +16,16 @@ impl Drop for Child {
     }
 }
 
+fn f1(p: Parent) {
+    println!("p1: {:?}", p);
+}
+
 fn main() {
     let mut p1 = Parent(1, Child(11), Child(12));
-    let p2 = p1;
-    println!("p2: {:?}", p2);
-    //  20 |     let mut p1 = Parent(1, Child(11), Child(12));
-    //     |         ------ move occurs because `p1` has type `Parent`, which does not implement the `Copy` trait
-    //  21 |     let p2 = p1;
-    //     |              -- value moved here
-    //  22 |     println!("p2: {:?}", p2);
-    //  23 |     println!("p1: {:?}", p1); 
+    f1(p1);
+    // println!("p1: {:?}", p1); // エラー
+    //  25 |     f1(p1);
+    //     |        -- value moved here
+    //  26 |     println!("p1: {:?}", p1); // エラー
     //     |                          ^^ value borrowed here after move
-    // println!("p1: {:?}", p1); // 上記のエラーがでる
 }
