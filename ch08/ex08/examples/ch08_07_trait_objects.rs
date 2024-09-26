eab24ad20fc5b1c5c5a81ac96457b35360a6be37
kimiyash <kimiyash@gmail.com>
2024-09-27 08:32:27 +0900

    ch08_07_trait_objects.rs

--- ch08/ex08/examples/ch08_07_trait_objects.rs
+++ ch08/ex08/examples/ch08_07_trait_objects.rs
@@ -1,7 +1,7 @@
 use std::string::ToString;
 use std::fmt::Display;
 
-fn stringfy<T: ToString>(t: T) -> String {
+fn stringfy(t: Box<dyn ToString>) -> String {
     t.to_string()
 }
 
@@ -14,8 +14,10 @@ fn stringfy_u64(t: u64) -> String {
 }
 
 fn main() {
-    stringfy(1_i32);
-    stringfy::<i32>(1_i32);
+    // stringfy(1_i32);
+    // stringfy::<i32>(1_i32);
+    stringfy(Box::new(1));
+    stringfy(Box::new("test".to_string()));
 
     stringfy_i32(1_i32);
     stringfy_u64(1_u64);
