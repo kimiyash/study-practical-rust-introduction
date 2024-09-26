6f0bc545c57be23d0f7b1305ed7731365277ac70
kimiyash <kimiyash@gmail.com>
2024-09-27 07:29:59 +0900

    ch08_06_overload.rs

--- ch08/ex08/examples/ch08_06_overload.rs
+++ ch08/ex08/examples/ch08_06_overload.rs
@@ -1,23 +1,20 @@
-trait Overload {
-    fn call(&self) -> &'static str;
+trait Overload<T> {
+    fn call(&self, t: T) -> &'static str;
 }
 
-impl Overload for i32 {
-    fn call(&self) -> &'static str {
-        "i32"
+impl Overload<i32> for i32 {
+    fn call(&self, _: i32) -> &'static str {
+        "(i32, i32)"
     }
 }
 
-impl Overload for str {
-    fn call(&self) -> &'static str {
-        "str"
+impl Overload <char> for i32 {
+    fn call(&self, _: char) -> &'static str {
+        "(i32, char)"
     }
 }
 
 fn main() {
-    assert_eq!(1_i32.call(), "i32");
-    assert_eq!("str".call(), "str");
-
-    assert_eq!(Overload::call(&1_i32), "i32");
-    assert_eq!(Overload::call("str"), "str");
+    assert_eq!(1_i32.call(2_i32), "(i32, i32)");
+    assert_eq!(1_i32.call('c'), "(i32, char)");
 }
