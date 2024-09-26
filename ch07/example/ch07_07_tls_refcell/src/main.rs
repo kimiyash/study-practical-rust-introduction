81a348199291c79ab5b92816f4e3882b8591e365
kimiyash <kimiyash@gmail.com>
2024-09-26 10:38:53 +0900

    ch07_07_tls_refcell

---
+++ ch07/example/ch07_07_tls_refcell/src/main.rs
@@ -0,0 +1,27 @@
+use std::cell::RefCell;
+use std::collections::HashSet;
+
+thread_local!(
+    static RABBITS: RefCell<HashSet<&'static str>> = {
+        let rb = ["ロップイヤー", "ダッチ"].iter().cloned().collect();
+        RefCell::new(rb)
+    }
+);
+
+fn main() {
+    RABBITS.with(|rb| {
+        assert!(rb.borrow().contains("ロップイヤー"));
+        rb.borrow_mut().insert("ネザーランド・ドワーフ");
+    });
+
+    std::thread::spawn(||
+        RABBITS.with(|rb| rb.borrow_mut().insert("ドワーフホト"))
+    ).join().expect("Thread error");
+
+    RABBITS.with(|rb| {
+        assert!(rb.borrow().contains("ネザーランド・ドワーフ"));
+        // RABBITSはスレッド事に持つので、別スレッドで追加した要素はここにいない
+        assert!(!rb.borrow().contains("ドワーフホト"));
+    });
+
+}
