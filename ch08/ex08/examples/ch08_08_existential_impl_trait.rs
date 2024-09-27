68fa8ffbf8ad1831743fb37ab033fc2e56478024
kimiyash <kimiyash@gmail.com>
2024-09-27 09:25:34 +0900

    ch08_08_existential_impl_trait.rs

--- ch08/ex08/examples/ch08_08_existential_impl_trait.rs
+++ ch08/ex08/examples/ch08_08_existential_impl_trait.rs
@@ -23,9 +23,47 @@ fn one() -> impl fmt::Display {
     1_i32
 }
 
+// 下記もエラー
+// fn one(is_float: bool) -> impl fmt::Display {
+//     if is_float {
+//         1.0_f32
+//     } else {
+//         1_i32
+// //      ^^^^^ expected `f32`, found `i32`
+//     }
+// }
+
+// クロージャは匿名型になるので引数の型が特定できない
+// fn gen_counter(init: i32) -> ??? {
+//     let mut n = init;
+//     move || {
+//         let ret = n;
+//         n += 1;
+//         ret
+//     }
+// }
+// こうなら書ける
+// fn gen_counter(init: i32) -> Box<dyn FnMut() -> i32> {
+//     let mut n = init;
+//     Box::new(move || {
+//         let ret = n;
+//         n += 1;
+//         ret
+//     })
+// }
+// トレイトオブジェクトを使わなくするには下記の様に書く
+fn gen_counter(init: i32) -> impl FnMut() -> i32 {
+    let mut n = init;
+    move || {
+        let ret = n;
+        n += 1;
+        ret
+    }
+}
+
 fn main() {
     let n = one();
-    println!("{}", n + n); // これはエラーになる
+    // println!("{}", n + n); // これはエラーになる
     // 28 |     println!("{}", n + n); // これはエラーになる
     //    |                    - ^ - impl std::fmt::Display
     //    |                    |
