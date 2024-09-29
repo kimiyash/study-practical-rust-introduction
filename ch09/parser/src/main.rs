af89b6224b22750b0b9105e883b950a9c0c3eeff
kimiyash <kimiyash@gmail.com>
2024-09-29 12:57:50 +0900

    parser

--- ch09/parser/src/main.rs
+++ ch09/parser/src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp::Eq;
 use std::cmp::PartialEq;
 use std::io;
+use std::iter::Peekable;
 
 /// 位置情報。.0から.1までの区間を現す
 /// 例えばLoc(4, 6)なら文字列の6文字目から7文字目までの区間を表す(0の始まり)
@@ -189,9 +190,7 @@ fn lex_number(input: &[u8], pos: usize) -> Result<(Token, usize), LexError> {
 
     let start = pos;
     // recognize_manyを使って数値を読む
-    let end = recognize_many(input, start, |b|
-        b"1234567890".contains(&b)
-    );
+    let end = recognize_many(input, start, |b| b"1234567890".contains(&b));
     // 数字の列を数値に変換する
     let n = from_utf8(&input[start..end])
         .unwrap()
@@ -203,9 +202,7 @@ fn lex_number(input: &[u8], pos: usize) -> Result<(Token, usize), LexError> {
 
 fn skip_space(input: &[u8], pos: usize) -> Result<((), usize), LexError> {
     // recgnize_manyを使って空白をとばす
-    let pos = recognize_many(input, pos, |b|
-        b" \n\t".contains(&b)
-    );
+    let pos = recognize_many(input, pos, |b| b" \n\t".contains(&b));
     Ok(((), pos))
 }
 
@@ -222,6 +219,174 @@ fn prompt(s: &str) -> io::Result<()> {
     let mut stdout = stdout.lock();
     stdout.write_all(s.as_bytes())?;
     stdout.flush()
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum AstKind {
+    /// 数値
+    Num(u64),
+    /// 単項演算
+    UniOp { op: UniOp, e: Box<Ast> },
+    /// 二項演算
+    BinOp { op: BinOp, l: Box<Ast>, r: Box<Ast> },
+}
+
+type Ast = Annot<AstKind>;
+
+// ヘルパメソッドを定義しておく
+impl Ast {
+    fn num(n: u64, loc: Loc) -> Self {
+        // impl<T> Annot<T> で実装した new を呼ぶ
+        Self::new(AstKind::Num(n), loc)
+    }
+
+    fn uniop(op: UniOp, e: Ast, loc: Loc) -> Self {
+        Self::new(AstKind::UniOp { op, e: Box::new(e) }, loc)
+    }
+
+    fn binop(op: BinOp, l: Ast, r: Ast, loc: Loc) -> Self {
+        Self::new(
+            AstKind::BinOp {
+                op,
+                l: Box::new(l),
+                r: Box::new(r),
+            },
+            loc,
+        )
+    }
+}
+
+/// 単行演算子を表すデータ型
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum UniOpKind {
+    /// 正号
+    Plus,
+    /// 負号
+    Minus,
+}
+
+type UniOp = Annot<UniOpKind>;
+
+impl UniOp {
+    fn plus(loc: Loc) -> Self {
+        Self::new(UniOpKind::Plus, loc)
+    }
+
+    fn minus(loc: Loc) -> Self {
+        Self::new(UniOpKind::Minus, loc)
+    }
+}
+
+// 二項演算子を表すデータ型
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum BinOpKind {
+    /// 加算
+    Add,
+    /// 減算
+    Sub,
+    /// 乗算
+    Mult,
+    /// 除算
+    Div,
+}
+
+type BinOp = Annot<BinOpKind>;
+
+impl BinOp {
+    fn add(loc: Loc) -> Self {
+        Self::new(BinOpKind::Add, loc)
+    }
+
+    fn sub(loc: Loc) -> Self {
+        Self::new(BinOpKind::Sub, loc)
+    }
+
+    fn mult(loc: Loc) -> Self {
+        Self::new(BinOpKind::Mult, loc)
+    }
+
+    fn div(loc: Loc) -> Self {
+        Self::new(BinOpKind::Div, loc)
+    } 
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum ParseError {
+    /// 予期しないトークンがきた
+    UnexpectedToken(Token),
+    /// 式を期待していたのに式でないものが来た
+    NotExpression(Token),
+    /// 演算子を期待していたのに演算子でないものが来た
+    NotOperator(Token),
+    /// カッコが閉じられていない
+    UnclosedOpenParen(Token),
+    /// 式の解析が終わったのにまだトークンが残っている
+    ReadundantExpression(Token),
+    /// パース途中で入力が終わった
+    Eof,
+}
+
+fn parse(tokens: Vec<Token>) -> Result<Ast, ParseError> {
+    // 入力をイテレータにし、Peekable にする
+    let mut tokens = tokens.into_iter().peekable();
+    // その後の parse_expr を呼んでエラーを処理する
+    let ret = parse_expr(&mut tokens)?;
+    match tokens.next() {
+        Some(tok) => Err(ParseError::ReadundantExpression(tok)),
+        None => Ok(ret),
+    }
+}
+
+fn parse_expr<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<Ast, ParseError>
+where
+    Tokens: Iterator<Item = Token>,
+{
+    // parse_expr は parse_expr3 を呼び出すだけ
+    parse_expr3(tokens)
+}
+
+fn parse_expr3<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<Ast, ParseError>
+where
+    Tokens: Iterator<Item = Token>,
+{
+    // 最初にEXPR3 ("+" | "-") EXPR2 を試す
+    // まずは EXPR3 をパースし
+    match parse_expr3(tokens) {
+        // 失敗したら parse_expr2 にフォールバック ( | EXPR2 の部分)
+        Err(_) => parse_expr2(tokens),
+        // 成功したら
+        Ok(e) => {
+            // peek で先読みして
+            match tokens.peek().map(|tok| tok.value) {
+                // ("+" | "-") であることを確認する | を使ってパターンマッチを複数並べられる
+                Some(TokenKind::Plus) | Some(TokenKind::Minus) => {
+                    // ("+" | "-" ) であれば入力を消費してパースを始める
+                    let op = match tokens.next().unwrap() {
+                        // Token は型エイリアスだがパターンマッチにも使える
+                        Token {
+                            // パターンマッチはネスト可能
+                            value: TokenKind::Plus,
+                            loc,
+                        } => BinOp::add(loc),
+                        Token {
+                            value: TokenKind::Minus,
+                            loc,
+                        } => BinOp::sub(loc),
+                        //入力が "+" か "-" であることは確認したのでそれ以外はありえない
+                        _ => unreachable!(,)
+                    };
+                    // EXPR2 をパース
+                    let r = parse_expr2(tokens)?;
+                    // 結果は加減
+                    let loc = e.loc.merge(&r.loc);
+                    Ok(Ast::binop(op, e, r, loc))
+                }
+                // それ以外はエラー。エラーの種類で処理をわける
+                Some(_) => Err(ParseError::UnexpectedToken(tokens.next().unwrap())),
+                None => Err(ParseError::Eof),
+            }
+        }
+    }
 }
 
 fn main() {
